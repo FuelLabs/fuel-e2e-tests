802e7bf6c9646d9b6a9286cfd4ef94292cda5d13
Ahmed Sagdati <ahmed.sagdati.ets@gmail.com>
2022-07-11 21:14:13 +0200

    tests passing, indication of macro hell letting up

---
+++ build.rs
@@ -0,0 +1,9 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("hello.rs");
+    println!("cargo:rerun-if-changed=build.rs");
+}
