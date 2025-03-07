4f55688b67d5efe540d5fa139d7cd4537150863a
Ahmed Sagdati <37515857+segfault-magnet@users.noreply.github.com>
2025-03-07 06:35:01 +0100

    chore: add balances test (#11)

---
+++ tests/limits.rs
@@ -0,0 +1,13 @@
+use fuel_e2e_tests::setup::{self, Setup};
+use fuels::prelude::*;
+
+// Because it has checks for indexation and it broke testnet once because the sdk wasn't
+// checking the flags and trying to paginate.
+#[tokio::test]
+async fn can_call_get_balances() -> color_eyre::Result<()> {
+    let Setup { wallet, .. } = setup::init().await?;
+
+    wallet.get_balances().await?;
+
+    Ok(())
+}
